83ac7b3659d3b789e2e665b2b8e3428d20e65c39
guilhermeolivsilva <guilherme.olivsilva01@gmail.com>
2023-05-18 13:56:49 -0300

    feat: added a script to export test env vars

---
+++ env_vars.sh
@@ -0,0 +1,4 @@
+export LLVM_PATH=/home/linuxbrew/.linuxbrew/opt/llvm@8/
+export CLANG_PATH=/home/linuxbrew/.linuxbrew/opt/llvm@8/bin/clang++
+export OPT_PATH=/home/linuxbrew/.linuxbrew/opt/llvm@8/bin/opt
+export LIB_EXTENSION=so
