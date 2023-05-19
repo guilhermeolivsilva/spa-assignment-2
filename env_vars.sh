934877fbb04d24eea5b68436248d732a42d3b2d1
guilhermeolivsilva <guilherme.olivsilva01@gmail.com>
2023-05-19 20:07:22 -0300

    fix: fixed the path to LLVM

--- env_vars.sh
+++ env_vars.sh
@@ -1,4 +1,4 @@
-export LLVM_PATH="/home/$(whoami)/llvm/"
+export LLVM_PATH="/home/$(whoami)/llvm/llvm"
 export CLANG_PATH="/home/$(whoami)/llvm/build/bin/clang++"
 export OPT_PATH="/home/$(whoami)/llvm/build/bin/opt"
 export LIB_EXTENSION=so
