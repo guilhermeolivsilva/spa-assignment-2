ce347cdce0749dac54c99b70c7ddff14fc86d80b
guilhermeolivsilva <guilherme.olivsilva01@gmail.com>
2023-05-25 21:00:26 -0300

    feat: added a debug variant for the run script

---
+++ debug_run.sh
@@ -0,0 +1,34 @@
+set -e
+
+echo "ENVIRONMENT"
+echo "Path to LLVM: $LLVM_PATH"
+echo "Library extension format: $LIB_EXTENSION"
+export LLVM_INCLUDE_DIRS="$LLVM_PATH/include/llvm"
+
+echo ""
+echo "Building the LLVM pass..."
+
+make clean
+cmake -DLLVM_INCLUDE_DIRS=$LLVM_INCLUDE_DIRS -G "Unix Makefiles" -B build/ .
+cmake --build build -j 4
+
+echo ""
+echo "Compiling tests and running the pass..."
+
+TESTS=(
+    "test_1"
+    "test_2"
+)
+
+for file in "${TESTS[@]}"
+do
+    echo "Running the pass for file $file"
+
+    clang++ -Xclang -disable-O0-optnone -S -O0 -emit-llvm tests/"$file.cpp" -o tests/"$file.ll"
+
+    # First optimization to enable the Range Analysis (otherwise, it will return [-inf, inf] for all the tested ranges)
+    opt -instnamer -mem2reg -break-crit-edges tests/"$file.ll" -S -o tests/"$file.ll"
+
+    # Run the Dead Code Elimination pass
+    opt -load "build/libRADeadCodeElimination.$LIB_EXTENSION" -vssa -client-ra -disable-output tests/"$file".ll 
+done
