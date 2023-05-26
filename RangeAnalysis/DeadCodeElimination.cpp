37bd660dc24a420d71e25e7e106008692869415b
guilhermeolivsilva <guilherme.olivsilva01@gmail.com>
2023-05-26 17:12:13 -0300

    feat: added additional predicate cases to the pass

--- RangeAnalysis/DeadCodeElimination.cpp
+++ RangeAnalysis/DeadCodeElimination.cpp
@@ -30,8 +30,19 @@ bool DeadCodeElimination::solveICmpInstruction(BranchInst* branchInst) {
 			}
 			break;
 
-		// if(x > y)
-		case CmpInst::ICMP_SGT:
+		// if(x < y) (unsigned)
+		case CmpInst::ICMP_ULT:
+			if (range1.getUpper().ult(range2.getLower())) {
+				eliminateCondition(branchInst, TRUE_BRANCH, I);
+				codeHasBeenEliminated = true;
+			} else if(range1.getLower().uge(range2.getUpper())) {
+				eliminateCondition(branchInst, FALSE_BRANCH, I);
+				codeHasBeenEliminated = true;
+			}
+			break;
+
+		// if(x <= y)
+		case CmpInst::ICMP_SLE:
 			if (range1.getUpper().sle(range2.getLower())) {
 				eliminateCondition(branchInst, TRUE_BRANCH, I);
 				codeHasBeenEliminated = true;
@@ -40,6 +51,84 @@ bool DeadCodeElimination::solveICmpInstruction(BranchInst* branchInst) {
 				codeHasBeenEliminated = true;
 			}
 			break;
+
+		// if(x <= y) (unsigned)
+		case CmpInst::ICMP_ULE:
+			if (range1.getUpper().ule(range2.getLower())) {
+				eliminateCondition(branchInst, TRUE_BRANCH, I);
+				codeHasBeenEliminated = true;
+			} else if(range1.getLower().ugt(range2.getUpper())) {
+				eliminateCondition(branchInst, FALSE_BRANCH, I);
+				codeHasBeenEliminated = true;
+			}
+			break;
+
+		// if(x > y)
+		case CmpInst::ICMP_SGT:
+			if (range1.getUpper().sgt(range2.getLower())) {
+				eliminateCondition(branchInst, TRUE_BRANCH, I);
+				codeHasBeenEliminated = true;
+			} else if(range1.getLower().sle(range2.getUpper())) {
+				eliminateCondition(branchInst, FALSE_BRANCH, I);
+				codeHasBeenEliminated = true;
+			}
+			break;
+
+		// if(x > y) (unsigned)
+		case CmpInst::ICMP_UGT:
+			if (range1.getUpper().ugt(range2.getLower())) {
+				eliminateCondition(branchInst, TRUE_BRANCH, I);
+				codeHasBeenEliminated = true;
+			} else if(range1.getLower().ule(range2.getUpper())) {
+				eliminateCondition(branchInst, FALSE_BRANCH, I);
+				codeHasBeenEliminated = true;
+			}
+			break;
+
+		// if(x >= y)
+		case CmpInst::ICMP_SGE:
+			if (range1.getUpper().sge(range2.getLower())) {
+				eliminateCondition(branchInst, TRUE_BRANCH, I);
+				codeHasBeenEliminated = true;
+			} else if(range1.getLower().slt(range2.getUpper())) {
+				eliminateCondition(branchInst, FALSE_BRANCH, I);
+				codeHasBeenEliminated = true;
+			}
+			break;
+
+		// if(x >= y) (unsigned)
+		case CmpInst::ICMP_UGE:
+			if (range1.getUpper().uge(range2.getLower())) {
+				eliminateCondition(branchInst, TRUE_BRANCH, I);
+				codeHasBeenEliminated = true;
+			} else if(range1.getLower().ult(range2.getUpper())) {
+				eliminateCondition(branchInst, FALSE_BRANCH, I);
+				codeHasBeenEliminated = true;
+			}
+			break;
+
+		// if(x == y)
+		case CmpInst::ICMP_EQ:
+			// Range class implements == operator. It's true if both bounds
+			// of the objects is exactly the same.
+			if (range1 == range2) {
+				eliminateCondition(branchInst, TRUE_BRANCH, I);
+			} else {
+				eliminateCondition(branchInst, FALSE_BRANCH, I);
+			}
+			codeHasBeenEliminated = true;
+			break;
+
+		// if(x != y)
+		case CmpInst::ICMP_NE:
+			if (range1 == range2) {
+				eliminateCondition(branchInst, FALSE_BRANCH, I);
+			} else {
+				eliminateCondition(branchInst, TRUE_BRANCH, I);
+			}
+			codeHasBeenEliminated = true;
+			break;
+
 		default:
 			break;
 	}
