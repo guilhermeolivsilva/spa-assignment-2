aeaae7c40524740bc0c54f1f3f8c5c57007973b2
guilhermeolivsilva <guilherme.olivsilva01@gmail.com>
2023-05-26 17:44:21 -0300

    chore: renamed the generated DeadCodeElimination lib to match the class name

--- run.sh
+++ run.sh
@@ -30,5 +30,5 @@ do
     opt -instnamer -mem2reg -break-crit-edges tests/"$file.ll" -S -o tests/"$file.ll"
 
     # Run the Dead Code Elimination pass
-    opt -load "build/libRADeadCodeElimination.$LIB_EXTENSION" -vssa -dead-code-elimination -stats tests/"$file".ll -o tests/"$file"_optimized.ll
+    opt -load "build/libDeadCodeElimination.$LIB_EXTENSION" -vssa -dead-code-elimination -stats tests/"$file".ll -o tests/"$file"_optimized.ll
 done
